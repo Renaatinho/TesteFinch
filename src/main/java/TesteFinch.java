7a3de878f735650af6aacc8ed93f37e15ec06d8b
Renato Nunes <renato.nunes@nextsi.com.br>
2019-02-17 11:55:45 -0300

    Inclusão das Threads

--- src/main/java/TesteFinch.java
+++ src/main/java/TesteFinch.java
@@ -7,76 +7,103 @@ import com.gargoylesoftware.htmlunit.html.*;
 import org.apache.commons.lang3.StringUtils;
 
 public class TesteFinch {
+
     public static void main(String[] args) {
 
-        Connection conn = null;
         try {
+            final Connection conn;
+            final Integer[] quantidadeThreadsEncerradas = {0};
+            final int quantidadeDiasPosteriores = 3;
+
             conn = DriverManager.getConnection("jdbc:mysql://localhost/TesteFinch?" +
                     "user=root&password=Renato1507");
 
-            java.util.Date data_atual = new Date();
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(data_atual);
-            String dia_atual = StringUtils.leftPad(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)),2,"0");
-            String mes_atual = StringUtils.leftPad(Integer.toString(cal.get(Calendar.MONTH) + 1),2,"0");
-            String ano_atual = Integer.toString(cal.get(Calendar.YEAR));
+            conn.setAutoCommit(false);
 
-            String baseUrl = "https://globoesporte.globo.com/placar-ge/"+ dia_atual + "-" + mes_atual + "-" + ano_atual + "/jogos.ghtml";
-            System.out.println(baseUrl);
-            WebClient client = new WebClient();
-            client.getOptions().setCssEnabled(false);
-            client.getOptions().setJavaScriptEnabled(false);
-            try {
+            for(int i=0; i<=quantidadeDiasPosteriores; i++){
 
-                HtmlPage page = client.getPage(baseUrl);
+                final Calendar cal = Calendar.getInstance();
 
-                List<HtmlArticle> jogoLista = page.getByXPath("//article[@class='card-jogo ']");
+                cal.setTime(new Date());
+                cal.add(Calendar.DATE, i);
 
-                if(!jogoLista.isEmpty()){
+                new Thread("TesteFinch" + i){
+                    public void run(){
 
-                    String query = " insert into partidas (id,data_partida,campeonato,time_mandante,placar_time_mandante,time_visitante,placar_time_visitante)"
-                            + " values (?, ?, ?, ?, ?, ?, ?)";
+                        String dia_atual = StringUtils.leftPad(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)),2,"0");
+                        String mes_atual = StringUtils.leftPad(Integer.toString(cal.get(Calendar.MONTH) + 1),2,"0");
+                        String ano_atual = Integer.toString(cal.get(Calendar.YEAR));
 
-                    for(HtmlArticle jogoItem : jogoLista){
-                        int id_partida = Integer.parseInt(jogoItem.getAttribute("id"));
-                        String data_partida = ((HtmlElement) jogoItem.getFirstByXPath(".//time[@class='hora-local']")).getAttribute("datetime");
-                        String campeonato = ((HtmlElement) jogoItem.getFirstByXPath(".//h1[@class='titulo local']/span")).asText();
-                        String time_mandante = ((HtmlElement) jogoItem.getFirstByXPath(".//div[@class='time mandante small-8 medium-10']/span[@class='nome nome-completo']")).asText();
-                        HtmlSpan span_placar_mandante = (HtmlSpan) jogoItem.getFirstByXPath(".//span[@class='placar placar-mandante']");
-                        String placar_time_mandante = span_placar_mandante != null ? span_placar_mandante.asText() : null;
-                        String time_visitante = ((HtmlElement) jogoItem.getFirstByXPath(".//div[@class='time visitante small-8 medium-10']/span[@class='nome nome-completo']")).asText();
-                        HtmlSpan span_placar_visitante = (HtmlSpan) jogoItem.getFirstByXPath(".//span[@class='placar placar-visitante']");
-                        String placar_time_visitante = span_placar_visitante != null ? span_placar_visitante.asText() : null;
-                        System.out.println(id_partida);
-                        System.out.println(data_partida);
-                        System.out.println(campeonato);
-                        System.out.println(time_mandante);
-                        System.out.println(placar_time_mandante);
-                        System.out.println(time_visitante);
-                        System.out.println(placar_time_visitante );
-                        System.out.println("---------------------------------------");
+                        String baseUrl = "https://globoesporte.globo.com/placar-ge/"+ dia_atual + "-" + mes_atual + "-" + ano_atual + "/jogos.ghtml";
+                        System.out.println(baseUrl);
+                        WebClient client = new WebClient();
+                        client.getOptions().setCssEnabled(false);
+                        client.getOptions().setJavaScriptEnabled(false);
+                        try {
 
-                        PreparedStatement preparedStmt = conn.prepareStatement(query);
-                        preparedStmt.setInt     (1, id_partida);
-                        preparedStmt.setString  (2, data_partida);
-                        preparedStmt.setString  (3, campeonato);
-                        preparedStmt.setString  (4, time_mandante);
-                        preparedStmt.setString  (5, placar_time_mandante);
-                        preparedStmt.setString  (6, time_visitante);
-                        preparedStmt.setString  (7, placar_time_visitante);
+                            HtmlPage page = client.getPage(baseUrl);
 
-                        preparedStmt.execute();
+                            List<HtmlArticle> jogoLista = page.getByXPath("//article[@class='card-jogo ']");
+
+                            if(!jogoLista.isEmpty()){
+
+                                String query = " REPLACE INTO partidas (id,data_partida,campeonato,time_mandante,placar_time_mandante,time_visitante,placar_time_visitante)"
+                                        + " VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+                                for(HtmlArticle jogoItem : jogoLista){
+                                    int id_partida = Integer.parseInt(jogoItem.getAttribute("id"));
+                                    String data_partida = ((HtmlElement) jogoItem.getFirstByXPath(".//time[@class='hora-local']")).getAttribute("datetime");
+                                    String campeonato = ((HtmlElement) jogoItem.getFirstByXPath(".//h1[@class='titulo local']/span")).asText();
+                                    String time_mandante = ((HtmlElement) jogoItem.getFirstByXPath(".//div[@class='time mandante small-8 medium-10']/span[@class='nome nome-completo']")).asText();
+                                    HtmlSpan span_placar_mandante = (HtmlSpan) jogoItem.getFirstByXPath(".//span[@class='placar placar-mandante']");
+                                    String placar_time_mandante = span_placar_mandante != null ? span_placar_mandante.asText() : null;
+                                    String time_visitante = ((HtmlElement) jogoItem.getFirstByXPath(".//div[@class='time visitante small-8 medium-10']/span[@class='nome nome-completo']")).asText();
+                                    HtmlSpan span_placar_visitante = (HtmlSpan) jogoItem.getFirstByXPath(".//span[@class='placar placar-visitante']");
+                                    String placar_time_visitante = span_placar_visitante != null ? span_placar_visitante.asText() : null;
+                                    System.out.println(id_partida);
+                                    System.out.println(data_partida);
+                                    System.out.println(campeonato);
+                                    System.out.println(time_mandante);
+                                    System.out.println(placar_time_mandante);
+                                    System.out.println(time_visitante);
+                                    System.out.println(placar_time_visitante );
+                                    System.out.println("---------------------------------------");
+
+                                    PreparedStatement preparedStmt = conn.prepareStatement(query);
+                                    preparedStmt.setInt     (1, id_partida);
+                                    preparedStmt.setString  (2, data_partida);
+                                    preparedStmt.setString  (3, campeonato);
+                                    preparedStmt.setString  (4, time_mandante);
+                                    preparedStmt.setString  (5, placar_time_mandante);
+                                    preparedStmt.setString  (6, time_visitante);
+                                    preparedStmt.setString  (7, placar_time_visitante);
+
+                                    preparedStmt.execute();
+                                }
+                            }
+
+                        } catch(Exception e) {
+                            try {
+                                conn.rollback();
+                            } catch (SQLException e1) {
+                                e1.printStackTrace();
+                            }
+                            e.printStackTrace();
+                        } finally {
+
+                            if(quantidadeDiasPosteriores == quantidadeThreadsEncerradas[0]){
+                                try {
+                                    conn.commit();
+                                    conn.close();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            }else{
+                                quantidadeThreadsEncerradas[0]++;
+                            }
+                        }
                     }
-                }
-
-            } catch(Exception e) {
-                e.printStackTrace();
-            }finally {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+                }.start();
             }
         } catch (SQLException e) {
             e.printStackTrace();
